bb561272beec12cdf5d18f135e1c54fac742c5cc
unknown <git+commit@lawrencelatif.com>
2018-07-21 09:14:15 -0700

    Initial commit

---
+++ host.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+##
+##  This script updates the DNS of a dynamic host using a file written by an SSH session
+##  v1.0 - 12 September 2016
+##
+
+CURRENT_IP="$(dig +short percy.oakland.lawrencelatif.com)"
+RECORDED_IP="$(cat /home/oakddns/oakland_ip.txt)"
+
+### domain settings
+DOMAIN="DOMAIN_NAME"
+SUBDOMAIN="SUBDOMAIN"
+
+### email message contents
+EMAIL_ADDRESS="EMAIL_ADDRESS"
+EMAIL_SUBJECT="[Success] Dynamic DNS update for HOSTNAME"
+EMAIL_BODY="The dynamic DNS for $SUBDOMAIN.$DOMAIN has been updated. It now resolves to $RECORDED_IP"
+
+  if [ "$CURRENT_IP" != "$RECORDED_IP" ]; then
+
+    ## first delete the current entry and then add it again with the new IP - change this to
+    ## update using dns -s "$DOMAIN" --list A,"$SUBDOMAIN","$RECORDED_IP"
+
+    /usr/local/psa/bin/dns -d "$DOMIAN" -a "$SUBDOMAIN" -ip "$CURRENT_IP"
+    /usr/local/psa/bin/dns -a "$DOMAIN" -a "$SUBDOMAIN" -ip "$RECORDED_IP"
+
+    NEW_RESOLVED_IP="$(dig +short percy.oakland.lawrencelatif.com)"
+
+    if [ "$RECORDED_IP" == "$NEW_RESOLVED_IP" ]; then
+
+      ## successful update, send email saying this has happened, record some stats
+      mail -s "$EMAIL_SUBJECT" "$EMAIL_ADDRESS" <<< "$EMAIL_BODY"
+
+    fi
+
+  fi
